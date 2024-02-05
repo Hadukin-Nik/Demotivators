85b9628a9cba6144a6040ef187ef603f141bf8c1
husan <husanov.nik@gmail.com>
2024-02-05 16:09:56 +0300

    Add usage of lombok and autowired by @Service annotation

--- src/main/java/com/demotivators/site/services/UserService.java
+++ src/main/java/com/demotivators/site/services/UserService.java
@@ -4,18 +4,15 @@ import com.demotivators.site.dao.UserDAO;
 import com.demotivators.site.dto.UserRegisterDTO;
 import com.demotivators.site.models.User;
 import com.demotivators.site.services.exceptions.UserDuplicateException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
     private final UserDAO userDAO;
-
-    @Autowired
-    public UserService(UserDAO userDAO) {
-        this.userDAO = userDAO;
-    }
 
     public User createUser(UserRegisterDTO userDTO) {
         User user = new User(userDTO.getLogin(), userDTO.getPassword());
